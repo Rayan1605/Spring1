5c5b32c13df7ae1dfe995bd1076b093fe88b735f
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-03 22:47:50 +0100

    Done

--- src/main/java/com/example/start2/Calculator/calculate.java
+++ src/main/java/com/example/start2/Calculator/calculate.java
@@ -1,11 +1,10 @@
 package com.example.start2.Calculator;
 
-import com.example.start2.Calculator.Answer;
-import com.example.start2.Calculator.CalculateInterface;
+
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 
